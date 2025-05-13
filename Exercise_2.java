576e49d803675bc6da72ecab96ef874362ebbdd7
Mounika8300 <88379577+Mounika8300@users.noreply.github.com>
2025-05-12 19:18:40 -0500

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,5 @@
+// Time complexity : O(n log n)
+//Space complexity : O(log n)
 class QuickSort 
 { 
     /* This function takes last element as pivot, 
@@ -7,12 +9,28 @@ class QuickSort
        pivot and all greater elements to right 
        of pivot */
     void swap(int arr[],int i,int j){
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
         //Your code here   
     }
     
     int partition(int arr[], int low, int high) 
     { 
    	//Write code here for Partition and Swap 
+   	    int p = arr[high];
+   	     int i = low - 1; // index of smaller element
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= p) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, high);
+
+        return i + 1;
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -20,11 +38,17 @@ class QuickSort
       high  --> Ending index */
     void sort(int arr[], int low, int high) 
     {  
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before and after partition
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
             // Recursively sort elements before 
             // partition and after partition 
     } 
   
-    /* A utility function to print array of size n */
     static void printArray(int arr[]) 
     { 
         int n = arr.length; 
@@ -33,7 +57,6 @@ class QuickSort
         System.out.println(); 
     } 
   
-    // Driver program 
     public static void main(String args[]) 
     { 
         int arr[] = {10, 7, 8, 9, 1, 5}; 
