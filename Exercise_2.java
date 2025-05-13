83182e570d7961f335c93a66607989a8f884a68e
Mounika8300 <88379577+Mounika8300@users.noreply.github.com>
2025-05-12 19:44:14 -0500

    Update Exercise_2.java

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,71 +1,51 @@
-// Time complexity : O(n log n)
-//Space complexity : O(log n)
-class QuickSort 
-{ 
-    /* This function takes last element as pivot, 
-       places the pivot element at its correct 
-       position in sorted array, and places all 
-       smaller (smaller than pivot) to left of 
-       pivot and all greater elements to right 
-       of pivot */
-    void swap(int arr[],int i,int j){
-        int temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-        //Your code here   
-    }
-    
-    int partition(int arr[], int low, int high) 
+// Unable to solve this problem, I was bit confused how to use stack instead of recursion
+class IterativeQuickSort { 
+    void swap(int arr[], int i, int j) 
     { 
-   	//Write code here for Partition and Swap 
-   	    int p = arr[high];
-   	     int i = low - 1; // index of smaller element
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+	//Try swapping without extra variable 
+    } 
+  
+    /* This function is same in both iterative and 
+       recursive*/
+    int partition(int arr[], int l, int h) 
+    { 
+        int pivot = arr[h]; 
+        int i = l - 1;
 
-        for (int j = low; j < high; j++) {
-            if (arr[j] <= p) {
+        for (int j = l; j < h; j++) {
+            if (arr[j] <= pivot) {
                 i++;
                 swap(arr, i, j);
             }
         }
-
-        swap(arr, i + 1, high);
-
+        swap(arr, i + 1, h);
         return i + 1;
     } 
-    /* The main function that implements QuickSort() 
-      arr[] --> Array to be sorted, 
-      low  --> Starting index, 
-      high  --> Ending index */
-    void sort(int arr[], int low, int high) 
-    {  
-        if (low < high) {
-            int pi = partition(arr, low, high);
-
-            // Recursively sort elements before and after partition
-            sort(arr, low, pi - 1);
-            sort(arr, pi + 1, high);
-        }
-            // Recursively sort elements before 
-            // partition and after partition 
-    } 
   
-    static void printArray(int arr[]) 
+    // Sorts arr[l..h] using iterative QuickSort 
+    void QuickSort(int arr[], int l, int h) 
     { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
-        System.out.println(); 
+        
+        //Try using Stack Data Structure to remove recursion.
     } 
   
+    // A utility function to print contents of arr 
+    void printArr(int arr[], int n) 
+    { 
+        int i; 
+        for (i = 0; i < n; ++i) 
+            System.out.print(arr[i] + " "); 
+    } 
+  
+    // Driver code to test above 
     public static void main(String args[]) 
     { 
-        int arr[] = {10, 7, 8, 9, 1, 5}; 
-        int n = arr.length; 
-  
-        QuickSort ob = new QuickSort(); 
-        ob.sort(arr, 0, n-1); 
-  
-        System.out.println("sorted array"); 
-        printArray(arr); 
+        IterativeQuickSort ob = new IterativeQuickSort(); 
+        int arr[] = { 4, 3, 5, 2, 1, 3, 2, 3 }; 
+        ob.QuickSort(arr, 0, arr.length - 1); 
+        ob.printArr(arr, arr.length); 
     } 
 } 
